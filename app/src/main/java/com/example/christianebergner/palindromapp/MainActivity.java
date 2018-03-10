9c6d5f8a2f03b65218bb0625817c959b4c7af985
Chrissi <chbergner@edu.aau.at>
2018-03-10 13:32:56 +0100

    Palindrom
    finished

---
+++ app/src/main/java/com/example/christianebergner/palindromapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.christianebergner.palindromapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.checkbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                check();
+            }
+        });
+        editText = findViewById(R.id.edit_text);
+    }
+
+
+    public void check(){
+        String s = editText.getText().toString();
+        if(s.isEmpty())
+            s="Geben Sie einen Text ein";
+        else if(s.length()>0 && s.length()<5)
+            s="Nicht genügend Buchstaben";
+        else{
+            boolean palindrom=s.equals(new StringBuilder(s).reverse().toString());
+            if(palindrom){
+                s="Ist ein Palindrom";
+            }else{
+                s="Ist kein Palindrom";
+            }
+        }
+
+        Toast toast = Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
